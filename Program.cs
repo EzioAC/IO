9978d5b902255425b7d4b44bdc9a0d51e00d113a
EzioXiang <1094708529@qq.com>
2017-11-07 22:31:22 +0800

    io init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new byte[1024];
+            var file = new FileStream("test1.txt", FileMode.Open);
+            while (file.Read(array, 0, 1024) > 0)
+            {
+                foreach(var i in array)
+                    Console.Write(i);
+            }
+            file.Close();
+        }
+    }
+}
